652a6365e4ee2a710903e1d27d55eb24a13ee0a7
Edwin Rubio <edwin@boltedtech.com>
2025-06-11 14:52:48 -0500

    refactor: restructure CarManager class and streamline car creation and movement logic

--- car_manager.py
+++ car_manager.py
@@ -4,22 +4,28 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.penup()
-        self.seth(180)
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        self.goto(self.random_location())
-        self.color(random.choice(COLORS))
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.seth(180)
+            new_car.goto(self.random_location())
+            new_car.color(random.choice(COLORS))
+            self.all_cars.append(new_car)
 
     def random_location(self):
         return (300, random.randint(-250, 250))
 
     def move(self):
-        self.forward(STARTING_MOVE_DISTANCE)
-    
+        for c in self.all_cars:
+            c.forward(STARTING_MOVE_DISTANCE)
+
     def inc_speed(self):
         global STARTING_MOVE_DISTANCE
         STARTING_MOVE_DISTANCE *= MOVE_INCREMENT
--- main.py
+++ main.py
@@ -16,31 +16,27 @@ player1 = Player()
 screen.onkeypress(player1.up, "Up")
 
 # Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre.
+cars = CarManager()
 
-cars = []
 game_is_on = True
 while game_is_on:
-    car = CarManager()
-    cars.append(car)
+    time.sleep(0.1)
+    screen.update()
 
-    for i in range(6):
-        for c in cars:
-            c.move()
-            time.sleep(0.1)
-            screen.update()
+    cars.create_car()
+    cars.move()
+
     # Detect when the turtle player collides with a car and stop the game if this happens.
-    for c in cars:
-        if c.distance(player1) < 40:
-            print(c)
+    for c in cars.all_cars:
+        if c.distance(player1) < 20:
             print("Game Over")
             game_is_on = False
-            #scoreboard = Scoreboard()
-            #scoreboard.game_over()
-    current_position = player1.position()
-    if current_position[1] >= 280:
-        player1.start()
-        cars.clear()
-        car.inc_speed()
-        screen.clear()
+    #         #scoreboard = Scoreboard()
+    #         #scoreboard.game_over()
+    # current_position = player1.position()
+    # if current_position[1] >= 280:
+    #     player1.start()
+    #     cars.clear()
+    #     screen.clear()
 
     # Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When this happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about creating an attribute and using the MOVE_INCREMENT to increase the car speed.
